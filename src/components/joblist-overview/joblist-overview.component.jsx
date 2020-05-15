259dacd38dbf16126dd73e321fb58840ef57caad
Falcon2 <hamzahmitou@gmail.com>
2020-05-15 16:34:40 +0300

    Adding the joblist overview + joblist item component

--- src/components/joblist-overview/joblist-overview.component.jsx
+++ src/components/joblist-overview/joblist-overview.component.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
 
+import Logo from '../../assets/photosnap.svg';
+
+import './joblist-overview.styles.scss';
+
 const JoblistOverview = () => {
     return (
         <div className="joblist-oveview">
-            joblist here
+            <div className="joblist-item">
+                <div className="logo-container">
+                    <img src={Logo} alt="photosnap" className="logo" />
+                </div>
+                <div className="job-features">
+                    <h4 className="company-name">Photosnap</h4>
+                    <span className="feature">
+                        new!
+                    </span>
+                    <span className="feature feature--dark">
+                        featured
+                    </span>
+                </div>
+                <div className="job-description">
+                    <h4 className="job-title">Senior Frontend Developer</h4>
+                    <ul className="job-details">
+                        <li>
+                            1d ago
+                        </li>
+                        <li>
+                            Full Time
+                        </li>
+                        <li>
+                            USA only
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
