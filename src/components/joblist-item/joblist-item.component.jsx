bf641eca01d8a5a1ac64b02f2c7e4f1c5f88eef3
unknown <hamzahmitou@gmail.com>
2020-05-22 14:05:57 +0300

    Fixed the logo issue

--- src/components/joblist-item/joblist-item.component.jsx
+++ src/components/joblist-item/joblist-item.component.jsx
@@ -3,27 +3,43 @@ import React from 'react';
 import JobFeatures from '../job-features/job-features.component';
 import Tags from '../tags/tags.component'
 
+
 import './joblist-item.styles.scss';
 
-const JoblistItem = ({company, logo,...job}) => {
-    return (
-        <div className="joblist-item">
-                <div className="job">
-                    <div className="logo-container">
-                        <img
-                            src={logo}
-                            className="logo"
-                            alt="logo"
+class JoblistItem extends React.Component {
+    constructor(props){
+        super();
+
+    }
+
+
+
+    render(){
+
+        const {company, logo, ...otherProps} = this.props;
+    
+        console.log();
+
+        return (
+            <div className="joblist-item">
+                    <div className="job">
+                        <div className="logo-container">
+                            <img
+                                src={require(`../../assets/${logo}`)}
+                                className="logo"
+                                alt="logo"
+                            />
+                        </div>
+                        <JobFeatures 
+                            company={company}
+                            {...otherProps}
                         />
-                    </div>
-                    <JobFeatures 
-                        company={company}
-                        {...job}
-                    />
-                    </div>
-                <Tags {...job} />
-            </div>
-    )
+                        </div>
+                    <Tags {...otherProps} />
+                </div>
+        )
+    }
+    
 }
 
 export default JoblistItem;
