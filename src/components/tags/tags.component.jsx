61a21b928d04810ea38ae039e9d0e4d3bdb5f9bb
unknown <hamzahmitou@gmail.com>
2020-05-23 19:46:40 +0300

    Switched to class component

--- src/components/tags/tags.component.jsx
+++ src/components/tags/tags.component.jsx
@@ -3,33 +3,39 @@ import React from 'react';
 import './tags.styles.scss';
 
 
-const Tags = ({role,level,languages,tools}) => {
-    return (
-        <div className="tag-list">
-            <span className="tag">
-                {role}
-            </span>
-            <span className="tag">
-                {level}
-            </span>
-           {languages ? 
-                 languages.map((lang, key) => (
-                    <span className="tag" key={key}>
-                        {lang}
-                    </span>)) : <span />
-            }
-            {
-                tools ? 
-                tools.map((tool, key) => (
-                    <span className="tag" key={key}>
-                        {tool}
-                    </span>)) : <span />
-            }
-           
-        </div>
-        
-        
-    )
+class Tags extends React.Component {
+    render(){
+
+        const {role, level, languages, tools, filter} = this.props; 
+
+
+        return (
+            <div className="tag-list">
+                <span className="tag" onClick={filter} >
+                    {role}
+                </span>
+                <span className="tag" onClick={filter}>
+                    {level}
+                </span>
+               {languages ? 
+                     languages.map((lang, key) => (
+                        <span className="tag" key={key} onClick={filter}>
+                            {lang}
+                        </span>)) : <span />
+                }
+                {
+                    tools ? 
+                    tools.map((tool, key) => (
+                        <span className="tag" key={key} onClick={filter}>
+                            {tool}
+                        </span>)) : <span />
+                }
+               
+            </div>
+            
+            
+        )
+    }
 }
 
 export default Tags;
