ce2b5f11cce160148cc6cfe57eba0e2753622e56
Falcon2 <hamzahmitou@gmail.com>
2020-05-26 09:48:44 +0300

    Optimizng data on mount

--- src/components/joblist-overview/joblist-overview.component.jsx
+++ src/components/joblist-overview/joblist-overview.component.jsx
@@ -6,27 +6,47 @@ import './joblist-overview.styles.scss';
 import Jobs from '../../data.json';
 import FiltersSection from '../filters-section/filters-section.component';
 
+import {optimizeJobs} from '../../utils/utils';
+
 class JoblistOverview extends React.Component {
     state = {
-        jobs: Jobs,
+        jobs: [],
         filters: []
     }
 
+    componentDidMount(){
+        this.setState({
+            jobs: optimizeJobs(Jobs)
+        });
+    }
+
+
+    
 
     onTagClick = (e) => {
         let filters = this.state.filters;
         if(!filters.includes(e.target.innerHTML)) {
             filters.push(e.target.innerHTML);
         }
+
+        let newJobs = this.state.jobs.filter(
+            job => filters.filter(element => job.reqs.indexOf(element) >= 0)
+        )
+
+        console.log(newJobs);
+
+
         this.setState({
-            filters
+            filters,
+           /*  jobs: newJobs */
         });        
     }
 
     onClearFilter = (e) => {
         e.preventDefault();
         this.setState({
-            filters: []
+            filters: [],
+            jobs: Jobs
         });
     }
 
@@ -41,6 +61,8 @@ class JoblistOverview extends React.Component {
 
     render(){
         const {jobs, filters} = this.state;
+
+
         return (
             <div className="joblist-oveview">
                 <FiltersSection 
